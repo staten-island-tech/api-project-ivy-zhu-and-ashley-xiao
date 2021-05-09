83e0160dd5173cfd1326f12b8fd3bef334e95e5d
IvyZ23 <ivy.zhu23@sitechhs.com>
2021-05-09 17:28:24 -0400

    -

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,5 +1,5 @@
 const DOMSelectors = {
-  grid: document.querySelector(".movie-grid"),
+  searchResult: document.querySelector(".search-results"),
 };
 
 export { DOMSelectors };
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,34 @@
 import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
 
 const key = "usCi4RaBNDKBfG3jWXiTgwjpfSJ6aWG4";
+
+const query = async function () {
+    try {
+        const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${key}`);
+        const data = await response.json();
+        const results = data.results;
+        results.books.forEach((book) => {
+            DOMSelectors.searchResult.insertAdjacentHTML("beforeend", 
+            `<div class="searched-books">
+            <img
+            src="${book.book_image}"
+            alt=""
+            class="book-cover">
+            <div class="book-info">
+                <h1 class="book-title">${book.title}</h1>
+                <p class="book-author">${book.contributor}</p>
+                <p class="book-description">
+                    ${book.description}
+                </p>
+                <div class="buy-btn">
+                    <a class="shop-btn" href="${book.amazon_product_url}">Buy Now</a></div>
+                </div>
+            </div>
+        </div>`);
+        });
+    } catch (error) {
+        console.log(error);
+        alert("something went wrong")
+    }
+};
+query();
